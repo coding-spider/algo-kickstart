d89afa20132105c9c61cf0ef8daeb95fc2fc98e3
Ro Han <rohank@valuefy.com>
2019-04-20 11:15:04 +0530

    added linked list class

--- ds/linked-list.js
+++ ds/linked-list.js
@@ -8,3 +8,20 @@ class Node {
     return this.value;
   }
 }
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    let newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    this.head.next = newNode;
+  }
+}
