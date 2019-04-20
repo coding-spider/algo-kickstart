f9a669486a69c3a1fc69fe2c3351673f838bec16
Ro Han <rohank@valuefy.com>
2019-04-20 11:27:19 +0530

    toArray and Prepend method

--- ds/linked-list.js
+++ ds/linked-list.js
@@ -24,4 +24,35 @@ class LinkedList {
 
     this.head.next = newNode;
   }
+
+  prepend(value) {
+    let newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  toArray() {
+    let nodes = [];
+    let current = this.head;
+    while (current != null) {
+      nodes.push(current.value);
+      current = current.next;
+    }
+    return nodes;
+  }
 }
+
+let linkedList = new LinkedList();
+
+linkedList.append(1);
+linkedList.append(2);
+console.log(linkedList);
+
+linkedList.prepend(3);
+console.log(linkedList.toArray());
